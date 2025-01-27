06bf72c553e3cd277375544d395f2061ae8e3a7e
HANS ELIJAH VIDUYA <2233053@slu.edu.ph>
2025-01-28 02:08:33 +0800

    Setup Hero Section

---
+++ aesir-portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import './assets/App.css'
+import HeroSection from './components/HeroSection'
+
+function App() {
+
+  return (
+      <>
+        <HeroSection/>
+      </>
+  )
+}
+
+
+export default App
